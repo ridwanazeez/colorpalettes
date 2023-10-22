873a983d772f4c44e224f0a670a573ccb19c26df
Ridwan Azeez <ridwanazeez2000@gmail.com>
2023-10-22 12:21:38 -0400

    add version

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/colourpalettes",
+  base: "/colourpalettes/",
   plugins: [vue()],
   resolve: {
     alias: {
